a7296f72bc3b99b6fc8e9008fab2df0cabbe12a8
Ben <ncor89b@gmail.com>
2023-06-19 22:17:35 +0200

    build(userModel.js): Finish implementing Models for Users and NFT's

--- Api/Model/userModel.js
+++ Api/Model/userModel.js
@@ -44,3 +44,41 @@ userSchema.pre("save", async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();{
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+  }
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  //: FALSE = NOT changed
+  return false;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
