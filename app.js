7a7be813921f4092d040d1a80361d3f00a0ad74f
Ben <ncor89b@gmail.com>
2023-06-21 20:20:21 +0200

    build(app.js) Build and implementing Routes & Middleware

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+//: IMPLEMENTING ROUTES
+const nftRouter = require('./Api/Routers/nftRouter');
+const userRouter = require('./Api/Routers/userRouter');
+
+
+//: IMPLEMENTING MIDDLEWARE
+const app = express();
+app.use(express.json({ limit: "100kb" }));
+
+app.use(cors());
+app.options("*", cors());
+
+//: ROUTES
+app.use("/api/v1/NFTs", nftRouter);
+app.use("/api/v1/user", userRouter);
+
+module.exports = app;
