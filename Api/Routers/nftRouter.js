76a204b8badce8cbb015c18ecbe8eb7c77c8a0c0
Ben <ncor89b@gmail.com>
2023-06-22 20:56:38 +0200

    refactor(nftRouter.js): Fix syntax issue

--- Api/Routers/nftRouter.js
+++ Api/Routers/nftRouter.js
@@ -3,8 +3,8 @@ const nftController = require("../Controllers/nftController");
 const router = express.Router();
 
 //: GET ALL NFTS AND POST REQUEST ON THE SAME ROUTER
-router.route("/").get(nftController.getAllNfts).post(nftController.createNft);
+router.route("/").get(nftController.getAllNFTs).post(nftController.createNft);
 
-router.route("/:id").get(nftController.getNft);
+router.route("/:id").get(nftController.getNFT);
 
 module.exports = router;
