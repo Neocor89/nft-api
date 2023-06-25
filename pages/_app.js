ec9015992e881c692178a963766817a5c02aa20b
Ben <ncor89b@gmail.com>
2023-06-25 19:03:31 +0200

    build(_app.js): Implementing Thirdweb provider for app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { StateContextProvider } from "../Context/NFTs";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider activeChain={ChainId.Mumbai}>
+      <StateContextProvider>
+        <Component {...pageProps} />
+      </StateContextProvider>
+    </ThirdwebProvider>
+  );
 }
