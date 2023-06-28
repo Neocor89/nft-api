b969558a4d53adec1cd38e11c1f6219b06c62f44
Ben <ncor89b@gmail.com>
2023-06-28 19:47:37 +0200

    build(Logo.jsx): Start of create and implemention Logo component of project

--- Components/Logo/Logo.jsx
+++ Components/Logo/Logo.jsx
@@ -1,7 +1,21 @@
+//: REACT MODULES
 import React from "react";
+import Link from "next/link";
+
+//: INTERNAL CSS MODULES
+import Style from "./Logo.module.css";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div class={Style.spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Logo;
