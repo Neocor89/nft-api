8d1742fde47e8aa13dfb15020aad84ac5ea67c21
Ben <ncor89b@gmail.com>
2023-07-03 21:02:28 +0200

    build(Footer.jsx): Completed creating footer component

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { RiSendPlaneFill } from "react-icons/ri";
+import { 
+  TiSocialFacebook,
+  TiSocialTwitter,
+  TiSocialLinkedin, 
+  TiSocialYoutube, 
+  TiSocialInstagram
+} from "react-icons/ti";
+
+//: INTERNAL IMPORTS CSS
+import Style from "./Footer.module.css";
+import { Logo } from "../index";
 
 const Footer = () => {
-  return <div>Footer</div>;
+
+  const menuList = ["Home", "About", "Product", "Contact", "ICO", "Menbership"];
+  return (
+    <div className={Style.footer}>
+      <div className={Style.footer_container}>
+        <div className={Style.footer_container_social}>
+          <a href="/">
+            <Logo className={Style.footer_container_social_logo} />
+          </a>
+          <p className={Style.footer_container_social_info}>
+            Digital Marketplace for NFT creators for switch anf changong your Crypto creation
+          </p>
+        <div className={Style.footer_social}>
+        <a href="#">
+          <TiSocialFacebook className={Style.social_icons} />
+        </a>
+        <a href="#">
+          <TiSocialLinkedin className={Style.social_icons} />
+        </a>
+        <a href="#">
+          <TiSocialTwitter className={Style.social_icons} />
+        </a>
+        <a href="#">
+          <TiSocialYoutube className={Style.social_icons} />
+        </a>
+        <a href="#">
+          <TiSocialInstagram className={Style.social_icons} />
+        </a>
+      </div>
+      </div>
+
+      <div className={Style.footer_container_help}>
+        <h3>Help Center</h3>
+        <div className={Style.menu}>
+          {menuList.map((elem, index) => (
+            <p key={index + 1}>{elem}</p>
+          ))}
+        </div>
+      </div>
+
+      <div className={Style.subscribe}>
+        <h3>Subscribe</h3>
+
+        <div className={Style.subscribe_container}>
+          <input 
+            type="email" 
+            name="Email Box" 
+            placeholder="Your_awesome-email *" 
+          />
+          <RiSendPlaneFill className={Style.subscribe_container_email} />
+        </div>
+        <div className={Style.subscribe_container_info}>
+          <p>Discover, collect, and sell your best NFT or all your collection OpenSea</p>
+        </div>
+      </div>
+      </div>
+    </div>
+
+  );
 };
 
 export default Footer;
