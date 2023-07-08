10e0e08004b90a2131ebb4c9a56df0818e340d44
Ben <ncor89b@gmail.com>
2023-07-08 23:08:32 +0200

    build(layout.js): Adding new Filter component in app

--- pages/layout.js
+++ pages/layout.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 //: INTERNAL COMPONENTS IMPORTS
-import { Logo, Button, Card, Footer, CheckBox } from "../Components";
+import { Logo, Button, Card, Footer, CheckBox, Filter } from "../Components";
 
 const layout = () => {
   return (
     <div className="home">
       <Logo />
       <Button />
+      <p>FILTER</p>
+      <Filter />
       <Card />
       <p>CHECKBOX</p>
       <CheckBox />
