07f6f9f415423beacd25d8ed626a6dd55ae418cb
Ben <ncor89b@gmail.com>
2023-07-14 16:29:42 +0200

    build(Notification.jsx): Finish create component

--- Components/Notification/Notification.jsx
+++ Components/Notification/Notification.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 //: INTERNAL STYLE IMPORT
-import Style from "./Notification.module.css"
+import Style from "./Notification.module.css";
 
-const Notification = () => {
-  return <div>Notification</div>;
+const Notification = ({setNotification, notification}) => {
+  return (
+    <div onClick={() => setNotification("")} class={Style.alert}>
+      {notification} Notification  
+      <span class={Style.close_button}>&times;</span>
+    </div>
+  );
 };
 
 export default Notification;
