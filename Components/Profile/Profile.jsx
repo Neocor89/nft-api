b1aa9ca5acec41f5005aa9318c50e07c350766ff
Ben <ncor89b@gmail.com>
2023-07-16 20:21:30 +0200

    build(Profile.jsx): Create user profile component

--- Components/Profile/Profile.jsx
+++ Components/Profile/Profile.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import Image from "next/image";
 
-const Profile = () => {
-  return <div>Profile</div>;
+//: INTERNAL CSS IMPORT
+import Style from "./Profile.module.css";
+import { YouTube, Twitter, Instagram, GitHub } from "../SVG";
+import images from "../Image/client/index";
+
+const Profile = ({ setOpenProfile, userBalance }) => {
+  return (
+    <>
+    <div class={Style.container}>
+    <div class={Style.image}>
+        <Image 
+          className="avatar_img"
+          style={{borderRadius: "50px"}}
+          src={images.client1}
+          width={80}
+          height={80}
+          onClick={() => setOpenProfile(true)}
+        />
+      </div>
+
+      <span>0xffeakgampjwdi...</span>
+      <p class={Style.info}>
+        {userBalance} HI, and Welcome to the NFTs place... Discover a platform that offers free decentralized storage and the ability to share your NFTs (non-fungible tokens) over IPFS (InterPlanetary File System) between creators. NFT data is stored by NFT. Storage and is accessible over the decentralized IPFS network.  
+      </p>
+
+      <div class={Style.share_container}>
+        <a href="https://github.com/Neocor89">
+          <GitHub />
+        </a>
+        <a href="https://twitter.com/Bendevweb89">
+          <Twitter />
+        </a>
+        <a href="https://instagram.com">
+          <Instagram />
+        </a>
+        <a href="https://www.youtube.com/@bendevweb89">
+          <YouTube />
+        </a>
+      </div>
+      <button 
+        class={Style.button}
+        onClick={() => setOpenProfile(false)}
+      >
+        Close
+      </button>
+    </div>
+    </>
+  );
 };
 
 export default Profile;
