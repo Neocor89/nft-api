087d5aecdcbc5dfe3f00572d3427596150e88bee
Ben <ncor89b@gmail.com>
2023-07-20 21:52:05 +0200

    build(Header.jsx): Start creating component with Login / Signup / Logout logical

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+//: INTERNAL IMPORTS
+import Style from "./Header.module.css";
+import { Logo, Login, SignUp } from "../index";
+
+
+const Header = ({ notification, setNotification }) => {
+  const menuList = [
+    {
+      name: 'Home',
+      link: "/",
+    },
+    {
+      name: 'About',
+      link: "#",
+    },
+    {
+      name: 'API',
+      link: "/nfts-api",
+    },
+  ];
+
+  const [signup, setSignup] = useState(false);
+  const [login, setLogin] = useState(false);
+  const [token, setToken] = useState("");
+
+  const openModel = (el) => {
+    if (el == "Login") {
+      setLogin(true);
+      setSignup(false);
+    } else if (el == "Signup") {
+      setSignup(true);
+      setLogin(false);
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("NFTApi token");
+    setToken(token);
+  }, [])
+  
+  const logout = (el) => {
+    const token = localStorage.getItem("NFTApi token");
+    window.location.reload();
+  }
+
+  return (
+    <div className={Style.container}>
+      <Logo />
+      <div className={Style.menu}>
+        {menuList.map((el, i) => (
+          <Link className={Style.link} href={el.href} key={i + 1}>
+            <p>{el.name}</p>
+          </Link>
+          // TODO
+          //+ RESTART HERE
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
