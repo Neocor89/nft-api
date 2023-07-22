5dd2429580f83c98d884ab24afd25615cf84d3ed
Ben <ncor89b@gmail.com>
2023-07-22 20:34:08 +0200

    refactor(Notification.jsx): Fix syntaxe error in file

--- Components/Notification/Notification.jsx
+++ Components/Notification/Notification.jsx
@@ -5,7 +5,7 @@ import Style from "./Notification.module.css";
 
 const Notification = ({setNotification, notification}) => {
   return (
-    <div onClick={() => setNotification("")} class={Style.alert}>
+    <div onClick={() => setNotification("")} className={Style.alert}>
       {notification} Notification  
       <span className={Style.close_button}>&times;</span>
     </div>
