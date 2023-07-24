37f3786286e88cace14e6d9392f49e8715f65008
Ben <ncor89b@gmail.com>
2023-07-24 19:30:05 +0200

    build(SignUp.jsx): Start build component add imports and internal imports

--- Components/SignUp/SignUp.jsx
+++ Components/SignUp/SignUp.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
+//:INTERNAL IMPORTS
+import Style from "./SignUp.module.css";
+import { FormSVG, Lock } from "../SVG";
+import Notification from "../index";
 
 const SignUp = () => {
   return <div>SignUp</div>;
