03f706ef404c2f36d4298cba61a5fcc2a82a2a7e
Ben <ncor89b@gmail.com>
2023-07-25 18:48:10 +0200

    build(SignUp.jsx): Implementation of user account creation logic completed

--- Components/SignUp/SignUp.jsx
+++ Components/SignUp/SignUp.jsx
@@ -26,22 +26,37 @@ const SignUp = ({ setLogin, setSignup, notification, setNotification }) => {
       user.password == "" || 
       user.passwordConfirm
     ) {
-      return setNotification("Please provide your all awesome detail!");
+      return setNotification("Please provide your all detail!");
     }
     setNotification("Wait your AWESOME account is being created...");
     try {
-      //: API IMPLEMENTATION
+      //: API CALL IMPLEMENTATION
       const response = await axios({
         method: "POST",
         url: `/api/v1/user/signup`,
         withCredentials: true,
-      })
+        data: {
+          name: user.name,
+          email: user.email,
+          password: user.password,
+          passwordConfirm: user.passwordConfirm
+        },
+      });
+      if (response.data.status == "success") {
+        setNotification("Your AWESOME account his successfully created");
+        localStorage.setItem("NFTApi Token", response.data.token);
+        setSignup(false);
+        setNotification("");
+        window.location.reload();
+      } else {
+        setNotification("Something went wrong, check and try again later")
+      }
     } catch (error) {
       console.error(error);
     }
-    // TODO
-    //+ RESTART HERE
-  }
+  };
+  // TODO
+  //+ RESTART HERE
   return <div>SignUp</div>;
 };
 
