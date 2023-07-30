df25dc82ad9f7a7748343adeff73ec5b8caf6814
Ben <ncor89b@gmail.com>
2023-07-30 15:13:32 +0200

    build(Upload.jsx): Logic for displaying Uploaded NFT images

--- Components/Upload/Upload.jsx
+++ Components/Upload/Upload.jsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
 import React from "react";
 
-const Upload = () => {
-  return <div>Upload</div>;
+//: INTERNAL IMPORTS
+import { Delete, File, UploadIcon } from "../SVG";
+import Style from "./Upload.module.css";
+
+
+const Upload = ({ onImageChange, display, retrieveFile }) => {
+  return (
+    <div className={Style.container}>
+      <div className={Style.header}>
+        {display == null ? (
+          <>
+            <UploadIcon />
+            <p>Browse file Upload!</p>
+          </>
+        ): (
+          <p>
+            <Image 
+              className={Style.image}
+              src={display}
+              alt="NFT image"
+              width={200}
+              height={200}
+            />
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Upload;
