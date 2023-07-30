357cb157aebab5491ee0cec0b2cca4a0456f506a
Ben <ncor89b@gmail.com>
2023-07-30 15:09:22 +0200

    build(File.jsx): Delete and create a new File icon, wait operation validation

--- Components/SVG/File.jsx
+++ Components/SVG/File.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 
-const File = () => {
+export default function File(props) {
   return (
-    <svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-      <g
-        id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></g>
-      <g id="SVGRepo_iconCarrier">
-        <path d="M15.331 6H8.5v20h15V14.154h-8.169z"></path>
-        <path d="M18.153 6h-.009v5.342H23.5v-.002z"></path>
-      </g>
-    </svg>
-  );
-};
+    <svg width="1em" height="1em" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path fill="#212325" d="M880 298.4H521L403.7 186.2a8.15 8.15 0 0 0-5.5-2.2H144c-17.7 0-32 14.3-32 32v592c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V330.4c0-17.7-14.3-32-32-32zM632 577c0 3.8-3.4 7-7.5 7H540v84.9c0 3.9-3.2 7.1-7 7.1h-42c-3.8 0-7-3.2-7-7.1V584h-84.5c-4.1 0-7.5-3.2-7.5-7v-42c0-3.8 3.4-7 7.5-7H484v-84.9c0-3.9 3.2-7.1 7-7.1h42c3.8 0 7 3.2 7 7.1V528h84.5c4.1 0 7.5 3.2 7.5 7v42z"></path>
+</svg>
+  )
+}
 
-export default File;
+// const File = () => {
+//   return (
+//     <svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+//       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+//       <g
+//         id="SVGRepo_tracerCarrier"
+//         stroke-linecap="round"
+//         stroke-linejoin="round"
+//       ></g>
+//       <g id="SVGRepo_iconCarrier">
+//         <path d="M15.331 6H8.5v20h15V14.154h-8.169z"></path>
+//         <path d="M18.153 6h-.009v5.342H23.5v-.002z"></path>
+//       </g>
+//     </svg>
+//   );
+// };
+
+// export default File;
