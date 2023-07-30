07e79d027af33308f0669e474f18e95cec8d269e
Ben <ncor89b@gmail.com>
2023-07-30 14:52:28 +0200

    build(UploadIcon.jsx): Remove et create new upload icon wait operation validation

--- Components/SVG/UploadIcon.jsx
+++ Components/SVG/UploadIcon.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 
-const Upload = () => {
+
+export default function Upload(props) {
   return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-      <g
-        id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></g>
-      <g id="SVGRepo_iconCarrier">
-        <path
-          d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15"
-          stroke="#000000"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>{" "}
-      </g>
-    </svg>
-  );
+    <svg width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path fill="#212325" d="M13 19v-4h3l-4-5l-4 5h3v4z"></path>
+    <path fill="#212325" d="M7 19h2v-2H7c-1.654 0-3-1.346-3-3c0-1.404 1.199-2.756 2.673-3.015l.581-.102l.192-.558C8.149 8.274 9.895 7 12 7c2.757 0 5 2.243 5 5v1h1c1.103 0 2 .897 2 2s-.897 2-2 2h-3v2h3c2.206 0 4-1.794 4-4a4.01 4.01 0 0 0-3.056-3.888C18.507 7.67 15.56 5 12 5C9.244 5 6.85 6.611 5.757 9.15C3.609 9.792 2 11.82 2 14c0 2.757 2.243 5 5 5z"></path>
+</svg>
+  )
 };
 
-export default Upload;
+// const Upload = () => {
+//   return (
+//     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+//       <g
+//         id="SVGRepo_tracerCarrier"
+//         stroke-linecap="round"
+//         stroke-linejoin="round"
+//       ></g>
+//       <g id="SVGRepo_iconCarrier">
+//         <path
+//           d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15"
+//           stroke="#000000"
+//           stroke-width="1.5"
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//         ></path>{" "}
+//       </g>
+//     </svg>
+//   );
+// };
+
+// export default Upload;
