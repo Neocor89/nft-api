b30d6f46933d768f1d2ace22b6345985e2d710d5
Ben <ncor89b@gmail.com>
2023-08-14 15:58:28 +0200

    build(Product.jsx): Adding externals imports

--- Components/Product/Product.jsx
+++ Components/Product/Product.jsx
@@ -1,6 +1,14 @@
 import { saveAs } from "file-saver";
 import React, { useState } from "react";
 
+//: INTERNAL IMPORTS
+import Style from "./Product.module.css";
+import BTNStyle from "../Button/Button.module.css";
+import images from "../Image/index";
+import client from "../Image/client";
+import { Donate } from "../index";
+
+
 const Product = () => {
   return <div>Product</div>;
 };
