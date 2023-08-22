eec78ff155ef38adc379f324f6389a8bd7f0f28c
Ben <ncor89b@gmail.com>
2023-08-22 19:16:35 +0200

    build(index.js): Adding internal imports

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,22 @@ import axios from "axios";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
+//: INTERAL IMPORTS
+import {
+  Card,
+  Upload,
+  Button,
+  Profile,
+  Header,
+  Footer,
+  Notification,
+  Logo,
+  Filter,
+  Form,
+} from "../Components";
+import { useStateContext } from "../Context/NFTs";
+import images from "../Components/Image/client/index";
+
 const index = () => {
   return <div>START BUILD INDEX PAGE</div>;
 };
