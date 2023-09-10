f34b11246d7dcb67f12fb31099f904fea131b6ce
Ben <ncor89b@gmail.com>
2023-09-10 20:38:45 +0200

    build(Card.jsx): Replacing false data with NFT data api

--- Components/Card/Card.jsx
+++ Components/Card/Card.jsx
@@ -5,20 +5,18 @@ import Link from "next/link";
 // INTERNAL STYLE AND COMPONENTS IMPORTS
 import Style from "./Card.module.css";
 import images from "../Image/client/index";
-import imagesNFT from "../Image/index";
+// import imagesNFT from "../Image/index";
 
 const Card = ({ setNotification, image, index }) => {
   return <div className={Style.card}>
     <div className={Style.content}>
-      {/* <a href={`/image/${image.imageID}`}> */}
-      <a href={`/image/1`}>
+      <a href={`/image/${image.imageID}`}>
         <p>
           {/* <img  */}
           <Image
             className={Style.image}
-            // src={image.image} 
-            src={imagesNFT.img1} 
-            // alt={image.owner}
+            src={image.image} 
+            alt={image.owner}
             width={250}
             height="200" 
           />
@@ -27,8 +25,7 @@ const Card = ({ setNotification, image, index }) => {
       <span className={Style.para}>
         <Image 
           className="avatar_image"
-          // src={images[`client${index + 1}`]}
-          src={images[`client1`]}
+          src={images[`client${index + 1}`]}
           width={50}
           height={50}
         />
@@ -37,28 +34,22 @@ const Card = ({ setNotification, image, index }) => {
           className={Style.para_small}
           onClick={() => (
             setNotification("Successfully Copied!"),
-            // navigator.clipboard.writeText(image.owner)
-            navigator.clipboard.writeText("Successfully Copied!")
+            navigator.clipboard.writeText(image.owner)
           )}
         >
-          0xffeakgampjwdi...
-          {/* {image.owner.slice(0, 25)}... */}
+          {image.owner.slice(0, 25)}...
         </small>
       </span>
 
       <span>
-        {/* CreatedAt : {new Date(image.createdAt * 1000).toDateString()} */}
-        CreatedAt : Juily 07 2023
-        {/* <small className={Style.number}>#{image.imageID}..</small> */}
-        <small className={Style.number}># 1..</small>
+        CreatedAt : {new Date(image.createdAt * 1000).toDateString()}
+        <small className={Style.number}>#{image.imageID}..</small>
       </span>
-        {/* <small className={Style.para}>{image.description.slice(0, 80)}..</small> */}
-        <small className={Style.para}>NFT most popular bofore APES MONKEYS..</small>
+        <small className={Style.para}>{image.description.slice(0, 80)}..</small>
         <button
           onClick={() => (
             setNotification("Image URL is Successfully Copied!"),
-            // navigator.clipboard.writeText(image.image)
-            navigator.clipboard.writeText("Image URL is Successfully Copied!")
+            navigator.clipboard.writeText(image.image)
           )}
           className={Style.button}
         >
