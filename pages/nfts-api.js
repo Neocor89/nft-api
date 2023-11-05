9eb4e78e4ff5de63ecba5118a68b45d1b59fe2c1
Ben <ncor89b@gmail.com>
2023-11-05 19:47:30 +0100

    build(nfts-api.js): Start creating page and add back to Home page link

--- pages/nfts-api.js
+++ pages/nfts-api.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const nftsapi = () => {
-  return <div>nftsapi</div>;
+  return (
+    <div>
+      <Link href="/">Home</Link>
+    </div>
+  );
 };
 
 export default nftsapi;
