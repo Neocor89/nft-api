80044fe7724e348fd994d11357c7562f01933ebc
Ben <ncor89b@gmail.com>
2023-11-05 19:49:40 +0100

    build(nfts-api.js): Adding Images of NFT market place

--- pages/nfts-api.js
+++ pages/nfts-api.js
@@ -1,10 +1,28 @@
 import Link from "next/link";
 import React from "react";
 
+//: INTERNAL IMPORTS
+import Images from "../Components/Image/index";
+import Image from "next/image";
+
 const nftsapi = () => {
+  const images = Object.values(Images).slice(0, 6);
+
   return (
     <div>
       <Link href="/">Home</Link>
+
+      <div>
+        {images.map((allImages, index) => (
+          <Image
+            key={index}
+            src={allImages}
+            alt={`Description de l'image`}
+            width={300}
+            height={300}
+          />
+        ))}
+      </div>
     </div>
   );
 };
