20c4998aa0185b4a6eb30f6c2fdaf9816a3b6928
Ben <ncor89b@gmail.com>
2023-11-11 22:49:01 +0100

    style(nfts-api.js): Starting implementing Bentogrids style

--- pages/nfts-api.js
+++ pages/nfts-api.js
@@ -4,28 +4,33 @@ import React from "react";
 //: INTERNAL IMPORTS
 import Images from "../Components/Image/index";
 import Image from "next/image";
+import AboutInfo from "/Components/Image/22.jpg";
+import AboutCreator from "/Components/Image/23.jpg";
 
 const nftsapi = () => {
   const images = Object.values(Images).slice(0, 6);
 
   return (
-    <div>
-      <Link href="/" className="home_link">
-        Home
-      </Link>
+    <>
+      <div className="nfts-api_container">
+        <Link href="/" className="home_link">
+          Home
+        </Link>
 
-      <div className="container">
-        {images.map((allImages, index) => (
-          <Image
-            key={index}
-            src={allImages}
-            alt={`Description de l'image`}
-            width={300}
-            height={300}
-          />
-        ))}
+        <div className="image_container">
+          {images.map((allImages, index) => (
+            <Image
+              key={index}
+              src={allImages}
+              alt={`Description de l'image`}
+              width={300}
+              height={300}
+              className="nfts_image"
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
