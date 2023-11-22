46877f4c65310d1ffb8260289f71072584956609
Ben <ncor89b@gmail.com>
2023-11-22 19:28:43 +0100

    build(AboutCreator.jsx + AboutCreator.css): Create dynamic Component and values for display all NFT's creators

---
+++ Components/AboutCreator/AboutCreator.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+
+export default function AboutCreator({image, about, name, logo}) {
+  return (
+    <div className="about_nfts-container">
+          <Image
+            src={image}
+            alt={`About NFTS creator`}
+            className="about_nfts-image"
+          />
+          <section className="about_nfts-content">
+            <h1 className="about_title">
+              {about}
+            </h1>
+            <div className="creator_content">
+              <Image
+                src={logo}
+                alt={`About NFTS creator`}
+                className="creator_logo-nfts"
+                width={45}
+                height={45}
+              />
+              <p className="creator">{name}</p>
+            </div>
+          </section>
+        </div>
+    );
+                {/* creations and NFTs designers. */}
+                {/* Welcome to the NFT market, where you can discover the latest */}
+};
