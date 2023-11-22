0ff603a42df5740a435e4774fa16a79938dac125
Ben <ncor89b@gmail.com>
2023-11-22 19:31:42 +0100

    build(nfts-api.js): Implementing AboutCreator component in page with dynamic values of NFT's creator

--- pages/nfts-api.js
+++ pages/nfts-api.js
@@ -5,7 +5,9 @@ import React from "react";
 import Images from "../Components/Image/index";
 import Image from "next/image";
 import AboutInfo from "/Components/Image/22.jpg";
-import AboutCreator from "/Components/Image/23.jpg";
+import Creator from "/Components/Image/23.jpg";
+//: COMPONENTS IMPORTS
+import AboutCreator from "../Components/AboutCreator/AboutCreator";
 
 const nftsapi = () => {
   const images = Object.values(Images).slice(0, 6);
@@ -17,29 +19,13 @@ const nftsapi = () => {
           Home
         </Link>
 
-        <div className="about_nfts-container">
-          <Image
-            src={AboutInfo}
-            alt={`About NFTS creator`}
-            className="about_nfts-image"
-          />
-          <section className="about_nfts-content">
-            <h1 className="about_title">
-              Welcome to the NFT market, where you can discover the latest
-              creations and NFTs designers.
-            </h1>
-            <div className="creator_content">
-              <Image
-                src={AboutCreator}
-                alt={`About NFTS creator`}
-                className="creator_logo-nfts"
-                width={45}
-                height={45}
-              />
-              <p className="creator">Ben webdeveloper</p>
-            </div>
-          </section>
-        </div>
+        <AboutCreator
+          image={AboutInfo}
+          about="Welcome to the NFT market, where you can discover the latest
+          creations and NFTs designers."
+          logo={Creator}
+          name="Ben webdeveloper"
+        />
 
         <div className="image_container">
           {images.map((allImages, index) => (
