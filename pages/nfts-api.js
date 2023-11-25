bc94aaf24decc1b18ce3082f9d2846da7a12fc46
Ben <ncor89b@gmail.com>
2023-11-25 19:44:06 +0100

    build(nfts-api.js): Modification of NFT image count display for creators

--- pages/nfts-api.js
+++ pages/nfts-api.js
@@ -6,11 +6,12 @@ import Images from "../Components/Image/index";
 import Image from "next/image";
 import AboutInfo from "/Components/Image/22.jpg";
 import Creator from "/Components/Image/23.jpg";
+
 //: COMPONENTS IMPORTS
 import AboutCreator from "../Components/AboutCreator/AboutCreator";
 
 const nftsapi = () => {
-  const images = Object.values(Images).slice(0, 6);
+  const images = Object.values(Images).slice(0, 9);
 
   return (
     <>
