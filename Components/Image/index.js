423af9f3640c65ef4bd6cec8b569f3129cdc4038
Ben <ncor89b@gmail.com>
2023-11-27 20:00:56 +0100

    style(Image): Implementing all images in import file

--- Components/Image/index.js
+++ Components/Image/index.js
@@ -7,6 +7,22 @@ import img6 from "./6.jpg";
 import img7 from "./7.jpg";
 import img8 from "./8.jpg";
 import img9 from "./9.jpg";
+import img10 from "./10.jpg";
+import img11 from "./11.jpg";
+import img12 from "./12.jpg";
+import img13 from "./13.jpg";
+import img14 from "./14.jpg";
+import img15 from "./15.jpg";
+import img16 from "./16.jpg";
+import img17 from "./17.jpg";
+import img18 from "./18.jpg";
+import img19 from "./19.jpg";
+import img20 from "./20.jpg";
+import img21 from "./21.jpg";
+import img22 from "./22.jpg";
+import img23 from "./23.jpg";
+import img24 from "./24.jpg";
+import img25 from "./25.jpg";
 
 //: ICONS
 import search from "./Search.png";
@@ -16,6 +32,7 @@ import Hide from "./Hide.png";
 
 //: HEADER LOGO
 import HeaderLogo from "./header-logo.png";
+
 export default {
   img1,
   img2,
@@ -26,6 +43,22 @@ export default {
   img7,
   img8,
   img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
   //: ICONS
   search,
   arrow,
